5e45d8ba343c3126db58c90ff40db795d8ea6a29
Sarp Ertoksoz <38987850+sarpertoksoz@users.noreply.github.com>
2018-12-01 09:30:30 +0300

    re 4

--- js/app.js
+++ js/app.js
@@ -1,6 +1,4 @@
-var siteWidth = 1280;
-var scale = screen.width /siteWidth
-document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
+document.width = screen.width;
 
 function linkclicked() {
 	document.getElementById('news-list').style.display = 'none';
