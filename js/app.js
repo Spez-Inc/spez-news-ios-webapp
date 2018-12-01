8662a1b1a18e81091a8c2d80150d9a9047a02b1d
Sarp Ertoksoz <38987850+sarpertoksoz@users.noreply.github.com>
2018-12-01 09:25:17 +0300

    re - 2

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+var siteWidth = 1280;
+var scale = screen.width /siteWidth
+document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
+
 function linkclicked() {
 	document.getElementById('news-list').style.display = 'none';
 	document.getElementById('web-iframe').style.display = 'block';
